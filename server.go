3999a1a28d2cf5c95ff2de7ea684bb93900d7b65
Artem Mikhalitsin <artemmikhalitsin@gmail.com>
2020-08-28 19:16:19 -0400

    Refactor tests

--- server.go
+++ server.go
@@ -22,5 +22,8 @@ func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
 
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	player := strings.TrimPrefix(r.URL.Path, "/players/")
+
+	w.WriteHeader(http.StatusNotFound)
+
 	fmt.Fprint(w, p.store.GetPlayerScore(player))
 }
--- server_test.go
+++ server_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -17,28 +18,51 @@ func TestGETPlayers(t *testing.T) {
 	server := &PlayerServer{store: &store}
 
 	t.Run("returns Pepper's score", func(t *testing.T) {
-		request, _ := http.NewRequest(http.MethodGet, "/players/Pepper", nil)
+		request := newGetScoreRequest("Pepper")
 		response := httptest.NewRecorder()
 
 		server.ServeHTTP(response, request)
 
 		assertResponseBody(t, response.Body.String(), "20")
+		assertStatusCode(t, response.Code, http.StatusOK)
 	})
 
 	t.Run("returns Floyd's score", func(t *testing.T) {
-		request, _ := http.NewRequest(http.MethodGet, "/players/Floyd", nil)
+		request := newGetScoreRequest("Floyd")
 		response := httptest.NewRecorder()
 
 		server.ServeHTTP(response, request)
 
 		assertResponseBody(t, response.Body.String(), "10")
+		assertStatusCode(t, response.Code, http.StatusOK)
+
 	})
+
+	t.Run("returns 404 on missing players", func(t *testing.T) {
+		request := newGetScoreRequest("Mario")
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		assertStatusCode(t, response.Code, http.StatusNotFound)
+	})
+}
+
+func newGetScoreRequest(name string) *http.Request {
+	request, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/players/%s", name), nil)
+	return request
 }
 
 func assertResponseBody(t *testing.T, got, want string) {
 	t.Helper()
 	if got != want {
 		t.Errorf("Got %q, want %q", got, want)
+	}
+}
+
+func assertStatusCode(t *testing.T, got, want int) {
+	if got != want {
+		t.Errorf("got status %d, want %d", got, want)
 	}
 }
 
