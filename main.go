6a98a64b4c955da7ce7872da3a3811425bdff700
Artem Mikhalitsin <artemmikhalitsin@gmail.com>
2020-08-29 16:31:45 -0400

    Change program to use Filesystem store

--- main.go
+++ main.go
@@ -3,12 +3,22 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
+const dbFileName = "game.db.json"
+
 func main() {
-	store := NewInMemoryPlayerStore()
+	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
+
+	if err != nil {
+		log.Fatalf("Problem opening %s: %v", dbFileName, err)
+	}
+
+	store := &FileSystemPlayerStore{db}
 	scoreServer := NewPlayerServer(store)
-	err := http.ListenAndServe(":5000", scoreServer)
+
+	err = http.ListenAndServe(":5000", scoreServer)
 	if err != nil {
 		log.Fatalf("Could not listen on port 5000 %v", err)
 	}
