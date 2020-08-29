3379ad316a1e9e6c71b085f27e82172745ad56ac
Artem Mikhalitsin <artemmikhalitsin@gmail.com>
2020-08-29 00:17:39 -0400

    Rework server constructor. Add Player type.

--- server_test.go
+++ server_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
@@ -16,7 +17,7 @@ func TestGETPlayers(t *testing.T) {
 		[]string{},
 	}
 
-	server := &PlayerServer{store: &store}
+	server := NewPlayerServer(&store)
 
 	tests := []struct {
 		name               string
@@ -61,7 +62,7 @@ func TestStoreWins(t *testing.T) {
 		map[string]int{},
 		[]string{},
 	}
-	server := &PlayerServer{store: &store}
+	server := NewPlayerServer(&store)
 
 	t.Run("it returns accepted on POST", func(t *testing.T) {
 		name := "Pepper"
@@ -79,6 +80,28 @@ func TestStoreWins(t *testing.T) {
 		if store.winCalls[0] != name {
 			t.Errorf("did not store correct winner got %q want %q", store.winCalls[0], name)
 		}
+	})
+}
+
+func TestLeague(t *testing.T) {
+	store := &StubPlayerStore{}
+	server := NewPlayerServer(store)
+
+	t.Run("it returns 200 on /league", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/league", nil)
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		var got []Player
+
+		err := json.NewDecoder(response.Body).Decode(&got)
+
+		if err != nil {
+			t.Fatalf("Unable to parse response from server %q into slive of Player", response.Body)
+		}
+
+		assertStatusCode(t, response.Code, http.StatusOK)
 	})
 }
 
